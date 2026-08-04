3bc30a0ba885ec703d0d78c13d7cd675ea485b0f
1ruz <samarawaithe@gmail.com>
2026-08-04 16:03:05 -0400

    df to csv file

--- src/load_data.py
+++ src/load_data.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+import numpy as np 
 from datetime import datetime, timedelta
 
 """ picking correlated tickers that may be interesting to take a lot at to begin with 
@@ -18,7 +19,16 @@ end_date = datetime.today().strftime("%Y-%m-%d")
 start_date = datetime.today() - timedelta(days=(365*2))
 
 df1 = yf.download(ticker_symbol1, start=start_date, end=end_date)
-print(df1.head())
+#print(df1.head())
+df1.to_csv("data/raw/{symbol}_raw.csv".format(symbol=ticker_symbol1))
 
 df2 = yf.download(ticker_symbol2, start=start_date,end=end_date)
-print(df2.head())
+#print(df2.head())
+df2.to_csv("data/raw/{symbol}_raw.csv".format(symbol=ticker_symbol2))
+
+
+"""""
+Data constitutes of : | Price | Close | High | Low | Open | Volume 
+                       Ticker | SYMBOL --------------------------- 
+                       Date   | 
+""" 
